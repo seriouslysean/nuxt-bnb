593564a2a89f5145e24f4c9ba0ca22d06e990c0d
Sean Kennedy <sean@codeserious.com>
2020-12-08 07:37:26 -0500

    09 - Creating the Property Page

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -1,6 +1,13 @@
 <template>
 <div>
-    {{ home.title }}
+    <div style="display:flex;">
+        <img v-for="image in home.images" :key="image" :src="image" height="200" width="150" />
+    </div>
+    {{ home.title }}<br>
+    ${{ home.pricePerNight }} / night<br>
+    <img src="/images/marker.svg" height="20" width="20" />{{ home.location.address }} {{ home.location.city }} {{ home.location.state }} {{ home.location.country }}<br>
+    <img src="/images/star.svg" height="20" width="20" />{{ home.reviewValue }}<br>
+    {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath
 </div>
 </template>
 
@@ -8,6 +15,11 @@
 import homes from '~/data/homes.json';
 
 export default {
+    head() {
+        return {
+            title: this.home.title,
+        }
+    },
     data() {
         return {
             home: {},
